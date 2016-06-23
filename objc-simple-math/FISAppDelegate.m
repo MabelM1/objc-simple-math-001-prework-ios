23d2507b768256157e5a5739728475c75cd711c8
Mabel <macmota@hotmail.com>
2016-06-23 19:15:03 -0400

    Done.

--- objc-simple-math/FISAppDelegate.m
+++ objc-simple-math/FISAppDelegate.m
@@ -7,11 +7,32 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your code here.
-     
-     */
+   
+    
+    NSUInteger u = 1;
+    
+    NSLog(@"u: %lu", u);
+    
+    u = 2 + 3;
+    
+    NSLog(@"u: %lu", u);
+    
+    u = 2 * 3;
+    
+    NSLog(@"u: %lu", u);
+    
+    //u = -1;
+    
+    //NSLog(@"u: %lu", u);
+    
+    //u = 8 - 10;
+    
+    //NSLog(@"u: %lu", u);
+    
+    
+    
+    
+    
     
     // do not alter
     return YES;  //
